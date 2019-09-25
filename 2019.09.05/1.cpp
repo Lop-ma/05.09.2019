386d20634324a576da26355d16cdf487277b7980
Lop-ma <55141590+Lop-ma@users.noreply.github.com>
2019-09-25 15:32:30 +0300

    Update 1.cpp

--- 2019.09.05/1.cpp
+++ 2019.09.05/1.cpp
@@ -1,1 +1,19 @@
-1
+#include <iostream>
+//Написать программу находящую среднее арифметическое факториалов последовательности чисел
+using namespace std;
+
+int main() 
+{
+	int n, s = 0, p = 1;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		for (int j = 1; j < a + 1; j++)
+			p = p * j;
+		s += p;
+		p = 1;
+	}
+	cout << s / n;
+	return 0;
+}
